7ae2f7d07f5116aae24ebd30c2381885246093a0
Thiago Alencar <talencar@gmail.com>
2025-01-28 14:33:45 -0300

    feat: add columns to cart

---
+++ db/migrate/20250128144838_add_columns_to_carts.rb
@@ -0,0 +1,6 @@
+class AddColumnsToCarts < ActiveRecord::Migration[7.1]
+  def change
+    add_column :carts, :last_interaction_at, :datetime
+    add_column :carts, :abandoned?, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_27_210953) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_28_144838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_27_210953) do
     t.decimal "total_price", precision: 17, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "last_interaction_at"
+    t.boolean "abandoned?", default: false
   end
 
   create_table "products", force: :cascade do |t|
